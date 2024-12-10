00eb58d4ff74fbc4af0867ab54f85eeb642f360f
frogsaurus <autobadin@gmail.com>
2024-12-10 19:02:37 +0700

    make a size

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+function populateBoard(size){
+//create sizexsize grid    
 let board = document.querySelector(".board");
-//create 16 columns & 1/16 width
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-//create row == 16x16
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 for(let i=0; i < 256; i++){
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
 }
+}
+
+populateBoard(16)
