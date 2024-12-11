b691f59ecc34f0e46817ca6000e0397de1b5f0b6
frogsaurus <autobadin@gmail.com>
2024-12-11 15:25:54 +0700

    add remove grid

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function populateBoard(size){
 //create sizexsize grid    
 let board = document.querySelector(".board");
+let squares = board.querySelectorAll("div");
+squares.forEach((div) => div.remove()); //remove the existing before create a new one
 board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for(let i=0; i < 256; i++){
+let amount = size*size;
+for(let i=0; i < amount; i++){
     let square = document.createElement("div");
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
 }
 }
 
-populateBoard(16)
+function changeSize(input){
+    if(input >=2 || input <= 100){
+        populateBoard(input)
+    }
+    else{
+        console.log("Too many squares");
+    }
+}
